69f2678382a8258173a06a85d1e808ee7ffc4e28
jasoncruse <jasonlonely@126.com>
2020-08-25 22:16:40 +0800

    css test

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function (err) {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log('Server is runing at 3000')
+    }
+});
