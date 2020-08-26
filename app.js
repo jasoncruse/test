4ce536710c577c6118ad90fe5856bee0b081a0c5
jasoncruse <jasonlonely@126.com>
2020-08-26 11:34:37 +0800

    remove edit test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.listen(3000, function (err) {
     if (err) {
         console.error(err)
